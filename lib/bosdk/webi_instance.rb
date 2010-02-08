721f4d012ed9ee8cc425972057169cbcb13750a5
Shane Emmons <semmons99@gmail.com>
2010-02-08 13:35:16 -0500

    added WebiInstance#objects

--- lib/bosdk/webi_instance.rb
+++ lib/bosdk/webi_instance.rb
@@ -5,5 +5,21 @@ module BOSDK
     def initialize(instance)
       @instance = instance
     end
+
+    def objects
+      return @objects unless @objects.nil?
+      objs = []
+      dict = @instance.getDictionary
+      (0 ... dict.getChildCount).each do |i|
+        obj = dict.getChildAt(i)
+        objs << {
+          :name => obj.getName,
+          :qual => obj.getQualification.getString.downcase.to_sym,
+          :type => obj.getType.getString.downcase.to_sym,
+          :object => obj,
+        }
+      end
+      @objects = objs
+    end
   end
 end
--- spec/webi_instance_spec.rb
+++ spec/webi_instance_spec.rb
@@ -14,5 +14,49 @@ module BOSDK
         @webi.instance.should == @document_instance
       end
     end
+
+    describe "#objects" do
+      before(:each) do
+        @report_dictionary = mock("ReportDictionary").as_null_object
+        @report_expression_1 = mock("ReportExpression").as_null_object
+        @report_expression_2 = mock("ReportExpression").as_null_object
+        @object_qualification_1 = mock("ObjectQualification").as_null_object
+        @object_qualification_2 = mock("ObjectQualification").as_null_object
+        @object_type_1 = mock("ObjectType").as_null_object
+        @object_type_2 = mock("ObjectType").as_null_object
+
+        @document_instance.should_receive(:getDictionary).once.with.and_return(@report_dictionary)
+
+        @report_dictionary.should_receive(:getChildCount).once.with.and_return(2)
+        @report_dictionary.should_receive(:getChildAt).once.with(0).and_return(@report_expression_1)
+        @report_dictionary.should_receive(:getChildAt).once.with(1).and_return(@report_expression_2)
+
+        @report_expression_1.should_receive(:getName).once.with.and_return("Object1")
+        @report_expression_1.should_receive(:getQualification).once.with.and_return(@object_qualification_1)
+        @report_expression_1.should_receive(:getType).once.with.and_return(@object_type_1)
+
+        @report_expression_2.should_receive(:getName).once.with.and_return("Object2")
+        @report_expression_2.should_receive(:getQualification).once.with.and_return(@object_qualification_2)
+        @report_expression_2.should_receive(:getType).once.with.and_return(@object_type_2)
+
+        @object_qualification_1.should_receive(:getString).once.with.and_return("DIMENSION")
+        @object_qualification_2.should_receive(:getString).once.with.and_return("MEASURE")
+
+        @object_type_1.should_receive(:getString).once.with.and_return("TEXT")
+        @object_type_2.should_receive(:getString).once.with.and_return("NUMERIC")
+      end
+
+      it "should return an array of report objects" do
+        @objects = [
+          {:name => 'Object1', :qual => :dimension, :type => :text, :object => @report_expression_1},
+          {:name => 'Object2', :qual => :measure, :type => :numeric, :object => @report_expression_2},
+        ]
+        @webi.objects.should == @objects
+      end
+
+      it "should cache results" do
+        2.times{@webi.objects}
+      end
+    end
   end
 end
