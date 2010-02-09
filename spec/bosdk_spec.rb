28264eb7e70c7ff4661b073d5488551aa4fc1b30
Shane Emmons <semmons99@gmail.com>
2010-02-09 08:29:19 -0500

    refactored 'bosdk_spec.rb'

--- spec/bosdk_spec.rb
+++ spec/bosdk_spec.rb
@@ -4,14 +4,16 @@ require 'bosdk'
 module BOSDK
   describe BOSDK do
     describe "#connect" do
-      specify "wraps EnterpriseSession#new in a closure" do
-        es = mock("EnterpriseSession").as_null_object
+      before(:each) do
+        @es = mock("EnterpriseSession").as_null_object
         class EnterpriseSession; end
-        EnterpriseSession.should_receive(:new).once.with('cms', 'Administrator', '', {:locale => "en_US"}).and_return(es)
-        es.should_receive(:disconnect).once.with.and_return
+        EnterpriseSession.should_receive(:new).once.with('cms', 'Administrator', '', {:locale => "en_US"}).and_return(@es)
+        @es.should_receive(:disconnect).once.with.and_return
+      end
 
+      specify "wraps EnterpriseSession#new in a closure" do
         BOSDK.connect('cms', 'Administrator', '', :locale => "en_US") do |session|
-          session.should == es
+          session.should == @es
         end
       end
     end
